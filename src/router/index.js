a6ff6b11c85502bdcee2381f5e1a37f848867748
gaozhichao <gaozhichao@wanda.cn>
2017-08-05 23:52:26 +0800

    [ADD]:增加
    [DES]:1. 2.

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-import Home from '@/components/Home'
 
 Vue.use(Router)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
   {
     path: '/Hello',
     name: 'Hello',
